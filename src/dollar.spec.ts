de6ce6b391777736687da099f7d43d2c322d6d85
rafaelsilva <todosomtodavoz@gmail.com>
2024-10-19 10:53:38 -0300

    test: dollar equality

--- src/dollar.spec.ts
+++ src/dollar.spec.ts
@@ -8,4 +8,9 @@ describe('Dollar', () => {
     products = five.times(3)
     expect(products.amount).toEqual(15)
   })
+
+  it('should return true when dollars are the same value are compared', async () => {
+    expect(new Dollar(5).equals(new Dollar(5))).toBeTruthy()
+    expect(new Dollar(5).equals(new Dollar(6))).toBeFalsy()
+  })
 })
