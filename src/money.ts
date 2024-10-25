606c4a901e5d3f70a1746db1a6216e6fb916cc60
rafaelsilva <todosomtodavoz@gmail.com>
2024-10-25 07:41:10 -0300

    feat: create factor methods for dollar and euro

--- src/money.ts
+++ src/money.ts
@@ -1,8 +1,12 @@
-export class Money {
+export abstract class Money {
   public amount: number
 
-  constructor(amount: number) {
-    this.amount = amount;
+  static dollar (amount: number): Money {
+    return new Dollar(amount)
+  }
+
+  static euro (amount: number): Money {
+    return new Euro(amount)
   }
 
   equals(other: Money): boolean {
@@ -10,5 +14,29 @@ export class Money {
       return false
     }
     return this.amount === other.amount
+  }
+
+  abstract times (multiplier: number): Money 
+}
+
+export class Dollar extends Money {
+  constructor (amount: number) {
+    super()
+    this.amount = amount
+  }
+
+  times (multiplier: number): Dollar {
+    return new Dollar(this.amount * multiplier)
+  }
+}
+
+export class Euro extends Money {
+  constructor(amount: number) {
+    super()
+    this.amount = amount
+  }
+
+  times (multiplier: number): Euro {
+    return new Euro(this.amount * multiplier)
   }
 }
