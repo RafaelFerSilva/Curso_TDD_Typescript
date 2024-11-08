cb8dffcf4a0ef9e876cf9cf807dde54e01694eb9
rafaelsilva <todosomtodavoz@gmail.com>
2024-11-08 07:25:15 -0300

    test: currency and constructors

--- src/money.spec.ts
+++ src/money.spec.ts
@@ -16,10 +16,13 @@ describe('Money', () => {
   it('should correctly money equality', async () => {
     expect(Money.dollar(5).equals(Money.dollar(5))).toBeTruthy()
     expect(Money.dollar(5).equals(Money.dollar(6))).toBeFalsy()
-
     expect(Money.euro(5).equals(Money.euro(5))).toBeTruthy()
     expect(Money.euro(5).equals(Money.euro(6))).toBeFalsy()
-
     expect(Money.euro(5).equals(Money.dollar(5))).toBeFalsy()
+  })
+
+  it("should correctly handle currencies", () => {
+    expect(Money.dollar(1).currency()).toEqual('USD')
+    expect(Money.euro(1).currency()).toEqual('EUR')
   })
 })
