5333ab494591c1e2455f400b893343ef8f41f935
rafaelsilva <todosomtodavoz@gmail.com>
2024-11-13 07:16:05 -0300

    feat: handle conversion rates in the bank

--- src/bank.ts
+++ src/bank.ts
@@ -1,8 +1,36 @@
 import { Expression } from "./expression";
 import { Money } from "./money";
 
+const currenciesTokey = (currencies: Pair): string => {
+  return `${currencies.from}-${currencies.to}`
+}
+
+interface Rates {
+  [index: string]: number
+}
+
+interface Pair {
+  from: string
+  to: string
+}
+
 export class Bank {
+  private rates: Rates = {}
+
   reduce (source: Expression, to: string): Money {
-    return source.reduce(to)
+    return source.reduce(this, to)
+  }
+
+  addRate(from: string, to: string, rate: number): void {
+    const currencies: Pair = { from, to}
+    this.rates[currenciesTokey(currencies)] = rate
+  }
+
+  rate (from: string, to: string): number {
+    if (from === to) {
+      return 1
+    }
+    const currencies: Pair = {from, to}
+    return this.rates[currenciesTokey(currencies)]
   }
 }
